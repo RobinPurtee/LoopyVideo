a73bfea5153c2c60644643de38f09f4b1dbac6e4
Rick Purtee <sourcetree@purtee.us>
2017-01-21 20:17:29 -0800

    last version of MediaSourceUri with it implemented using a Property

--- LoopyVideo/MediaSourceUri.cs
+++ LoopyVideo/MediaSourceUri.cs
@@ -9,32 +9,42 @@ using LoopyVideo.Logging;
 
 namespace LoopyVideo
 {
-    class MediaSourceUri : INotifyPropertyChanged
+    class MediaSourceUri
     {
         private Logger _log = new Logger("MediaSourceUri");
 
         public const string MediaUriName = "MediaUri";
         private const string settingsName = "MediaSource";
 
-        #region Singleton implementation
         private Uri _mediaUri;
-        private static readonly Lazy<MediaSourceUri> _instance = new Lazy<MediaSourceUri>(() => new MediaSourceUri());
-
-        /// <summary>
-        /// The property factory
-        /// </summary>
-        public static MediaSourceUri Instance
+        public Uri MediaUri
         {
-            get { return _instance.Value; }
+            get
+            {
+                if (_mediaUri == null)
+                {
+                    Uri newValue;
+                    string uriString = CurrentSetting;
+
+                    if (string.IsNullOrEmpty(uriString)
+                        || !Uri.TryCreate(uriString, UriKind.RelativeOrAbsolute, out newValue))
+                    {
+                        newValue = GetDefaultMediaUri();
+                    }
+                    _mediaUri = newValue;
+                }
+                _log.Information($"Media Uri: {_mediaUri.AbsolutePath}");
+                return _mediaUri;
+            }
+            set
+            {
+                if (_mediaUri != null && !value.Equals(_mediaUri))
+                {
+                    _mediaUri = value;
+                    CurrentSetting = value.ToString();
+                }
+            }
         }
-
-        /// <summary>
-        /// Private constructor to force singleton use
-        /// </summary>
-        private MediaSourceUri() { }
-        #endregion
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
 
         private string CurrentSetting
@@ -50,10 +60,10 @@ namespace LoopyVideo
         /// <returns></returns>
         private StorageFile GetDefaultMediaStorageFileAsync()
         {
-            StorageLibrary lib = StorageLibrary.GetLibraryAsync(KnownLibraryId.Videos).AsTask<StorageLibrary>().Result;
+            StorageLibrary lib = StorageLibrary.GetLibraryAsync(KnownLibraryId.Videos).GetAwaiter().GetResult();
             StorageFolder folder = lib.SaveFolder;
             // Get the files in the SaveFolder folder.
-            IReadOnlyList<StorageFile> filesList = folder.GetFilesAsync().AsTask<IReadOnlyList<StorageFile>>().Result;;
+            IReadOnlyList<StorageFile> filesList = folder.GetFilesAsync().GetAwaiter().GetResult();
             if (filesList.Count == 0)
             {
                 throw new FileNotFoundException("There are no files in the video library");
@@ -69,45 +79,6 @@ namespace LoopyVideo
         {
             return new Uri(GetDefaultMediaStorageFileAsync().Path);
         }
-
-        /// <summary>
-        /// Get the current MediaUri for the application
-        /// </summary>
-        public Uri Get()
-        {
-            if (_mediaUri == null)
-            {
-                Uri newValue;
-                string uriString = CurrentSetting;
-
-                if (string.IsNullOrEmpty(uriString) 
-                    || !Uri.TryCreate(uriString, UriKind.RelativeOrAbsolute, out newValue))
-                {
-                    newValue = GetDefaultMediaUri();
-                }
-                _mediaUri = newValue;
-            }
-            _log.Information($"Media Uri: {_mediaUri.AbsolutePath}");
-            return _mediaUri;
-        }
-
-        /// <summary>
-        /// Set the medai Uri to the give value
-        /// </summary>
-        /// <param name="value">the value to set the Property too</param>
-        /// <returns>True if the given value is a new one</returns>
-        public bool Set(Uri value)
-        {
-            bool hasChanged = _mediaUri != null && !value.Equals(_mediaUri);
-            if (hasChanged)
-            {
-                _mediaUri = value;
-                CurrentSetting = value.ToString();
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(MediaUriName));
-            }
-            return hasChanged;
-        }
-
 
 
     }
